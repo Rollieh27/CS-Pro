0ac249d0ab706ff1bb91fbd535850ab4549d4a78
Rollieh27 <119282670+Rollieh27@users.noreply.github.com>
2023-06-01 18:27:02 -0400

    fixed webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,65 +3,65 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install'],
-      }),
-      new WebpackPwaManifest({
-        name: 'Code Stack Pro',
-        short_name: 'CS Pro',
-        description: 'CS Pro is the best text editor available',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: 'src-sw.js',
-        swDest: 'src-sw.js',
-      }),
-    ],
+    plugins: [ new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'J.A.T.E'
+    }),
 
-    module: {
-      rules: [
+    new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Code Stack Pro',
+      short_name: 'CS Pro',
+      description: 'Just Another Text Editor',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: './',
+      publicPath: './',
+      icons: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
+    }),
+  ],
+
+    modules: {
+      rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    ],
     },
   };
 };
